ff7bbd6e4ce11e4c0472010dddf7c16c3dac61e2
Olav Vitters <ovitters@src.gnome.org>
2007-09-09 17:41:08 +0000

    Retab and fix SQL insertion
    svn path=/trunk/; revision=50

--- www/update_foundationmember.php
+++ www/update_foundationmember.php
@@ -9,167 +9,168 @@ define('SESSIONID', 'update_foundationmember');
 define('GROUP', 'membctte');
 
 class UpdateFoundationMember {
-	// Details for the mirror being updated
-	var $foundationmember;
+    // Details for the mirror being updated
+    var $foundationmember;
 
-	var $error;
-		
-	function UpdateFoundationMember($id) {
-		global $affectedgroups;
+    var $error;
+        
+    function UpdateFoundationMember($id) {
+        global $affectedgroups;
 
-		$foundationmember = FoundationMember::fetchmember($id);
-		if(!is_a($foundationmember, "FoundationMember")) {
-			$this->error = $foundationmember;
-			return;
-		}
+        $foundationmember = FoundationMember::fetchmember($id);
+        if(!is_a($foundationmember, "FoundationMember")) {
+            $this->error = $foundationmember;
+            return;
+        }
 
-		$this->foundationmember = $foundationmember;
-	}
-		
-	function main() {
-		global $config;
+        $this->foundationmember = $foundationmember;
+    }
+        
+    function main() {
+        global $config;
 
-		// Check session for previous instance
-		$container = $_SESSION[SESSIONID];
-		if(!is_a($container, "UpdateFoundationMember") || isset($_REQUEST['id'])) {
-			$id = $_REQUEST['id'];
-			$container = new UpdateFoundationMember($id);
-			$_SESSION[SESSIONID] = $container;
-		}
+        // Check session for previous instance
+        $container = $_SESSION[SESSIONID];
+        if(!is_a($container, "UpdateFoundationMember") || isset($_REQUEST['id'])) {
+            $id = $_REQUEST['id'];
+            $container = new UpdateFoundationMember($id);
+            $_SESSION[SESSIONID] = $container;
+        }
 
-		// Set up a page for tracking the response for this request
-		$page = new Page(STYLESHEET);
-		
-		// Service the request, tracking results and output on the given DOM
-		$container->service($page->result);
-		
-		// Send the page for post-processing and output
-		$page->send();
-		
-		// Save anything changed in the session
-		$_SESSION[SESSIONID] = $container;
-	}
-	
-	function service(&$dom) {
-		// A page node is mandatory
-		$dom->appendChild($pagenode = $dom->createElement("page"));
-		$pagenode->setAttribute("title", "Update Foundation Member '".$this->foundationmember->id."'");
+        // Set up a page for tracking the response for this request
+        $page = new Page(STYLESHEET);
+        
+        // Service the request, tracking results and output on the given DOM
+        $container->service($page->result);
+        
+        // Send the page for post-processing and output
+        $page->send();
+        
+        // Save anything changed in the session
+        $_SESSION[SESSIONID] = $container;
+    }
+    
+    function service(&$dom) {
+        // A page node is mandatory
+        $dom->appendChild($pagenode = $dom->createElement("page"));
+        $pagenode->setAttribute("title", "Update Foundation Member '".$this->foundationmember->id."'");
 
-		// Security check
-		if(!check_permissions($dom, $pagenode, GROUP)) return;
+        // Security check
+        if(!check_permissions($dom, $pagenode, GROUP)) return;
 
-		// Start the page off		
-		$formnode = $pagenode->appendChild($dom->createElement("updatefoundationmember"));
+        // Start the page off       
+        $formnode = $pagenode->appendChild($dom->createElement("updatefoundationmember"));
 
-		// If posting details, attempt to add the new details
-		if($_SERVER['REQUEST_METHOD'] == 'POST') {
-			$this->process($dom, $formnode);
-		}
-		
-		// Add current details to form
-		$this->foundationmember->add_to_node($dom, $formnode);
-		
-		// Add initialisation error, if any
-		if(PEAR::isError($this->error)) {
-			$node = $formnode->appendChild($dom->createElement("error"));
-			$node->appendChild($dom->createTextNode($this->error->getMessage()));
-		}
+        // If posting details, attempt to add the new details
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $this->process($dom, $formnode);
+        }
+        
+        // Add current details to form
+        $this->foundationmember->add_to_node($dom, $formnode);
+        
+        // Add initialisation error, if any
+        if(PEAR::isError($this->error)) {
+            $node = $formnode->appendChild($dom->createElement("error"));
+            $node->appendChild($dom->createTextNode($this->error->getMessage()));
+        }
 
-		return;
-	}
+        return;
+    }
 
-	function process(&$dom, &$formnode) {	
-		// Check ref (in case of multiple open pages)
-		$idcheck = $_POST['idcheck'];
-		if($this->foundationmember->id != $idcheck) {
-			$foundationmember = FoundationMember::fetchmirror($id);
-			if(!is_a($foundationmember, "FoundationMember")) {
-				$this->error = $foundationmember;
-				return;
-			}
-			$this->foundationmember = $foundationmember;
-		}
-		
-		// Read form and validate
-		$formerrors = $this->readform();
-		if(count($formerrors) > 0) {
-			foreach($formerrors as $error) {
-				$node = $formnode->appendChild($dom->createElement("formerror"));
-				$node->setAttribute("type", $error);
-			}
-			return;
-		}
+    function process(&$dom, &$formnode) {   
+        // Check ref (in case of multiple open pages)
+        $idcheck = $_POST['idcheck'];
+        if($this->foundationmember->id != $idcheck) {
+            $foundationmember = FoundationMember::fetchmirror($id);
+            if(!is_a($foundationmember, "FoundationMember")) {
+                $this->error = $foundationmember;
+                return;
+            }
+            $this->foundationmember = $foundationmember;
+        }
+        
+        // Read form and validate
+        $formerrors = $this->readform();
+        if(count($formerrors) > 0) {
+            foreach($formerrors as $error) {
+                $node = $formnode->appendChild($dom->createElement("formerror"));
+                $node->setAttribute("type", $error);
+            }
+            return;
+        }
 
-		// Attempt MySQL update
-		$result = $this->foundationmember->update();
-		if(PEAR::isError($result)) {
-			$node = $formnode->appendChild($dom->createElement("error"));
-			$node->appendChild($dom->createTextNode($result->getMessage()));
-			return;
-		}
-		
-		// Report success
-		if(is_array($result)) {
-			if ($this->foundationmember->renew) {  
-	      $membername = $this->foundationmember->firstname.' '. $this->foundationmember->lastname;
-  	    $to =  $this->foundationmember->email;
-    	  $cc = 'membership-committee@gnome.org';
-      	$subject = "GNOME Foundation Membership - Renewal accepted";
-	      $body = file_get_contents('renewal-template.txt');
-  	    // Replacing member name to template-mail body
-	      $body = str_replace('<member>', $membername, $body);
-  	    $recipients = array ($to, $cc);
-    	  $mime = new Mail_Mime();
-	      $mime->setTXTBody($body);
-  	    $headers = array(
-    	    "Reply-To" => "<membership-committee@gnome.org>",
-      	  "From" => "GNOME Foundation Membership Committee <membership-committee@gnome.org>",
-	        "To" => $to,
-  	      "Cc" => $cc,
-    	    "Subject" => $subject,
-      	);
-		$params = array(
-			'head_charset' => 'UTF-8',
-			'head_encoding' => 'quoted-printable',
-			'text_charset' => 'UTF-8',
-		);
-	      $content = $mime->get($params);
-	      $headers = $mime->headers($headers);
-        $mail = &Mail::factory('smtp');
-        $error = $mail->send($recipients, $headers, $content);
-        if(PEAR::isError($error))
-          return $error;
-				
-				$updatednode = $formnode->appendChild($dom->createElement('emailsent'));
-				
-			}
-				$updatednode = $formnode->appendChild($dom->createElement("updated"));
-				foreach($result as $change) {
-				$node = $updatednode->appendChild($dom->createElement("change"));
-				$node->setAttribute("id", $change);
-			}
-		}
+        // Attempt MySQL update
+        $result = $this->foundationmember->update();
+        if(PEAR::isError($result)) {
+            $node = $formnode->appendChild($dom->createElement("error"));
+            $node->appendChild($dom->createTextNode($result->getMessage()));
+            return;
+        }
+        
+        // Report success
+        if(is_array($result)) {
+            if ($this->foundationmember->renew) {  
+                $membername = $this->foundationmember->firstname.' '. $this->foundationmember->lastname;
+                $to =  $this->foundationmember->email;
+                $cc = 'membership-committee@gnome.org';
+                $subject = "GNOME Foundation Membership - Renewal accepted";
+                $body = file_get_contents('renewal-template.txt');
+            
+                // Replacing member name to template-mail body
+                $body = str_replace('<member>', $membername, $body);
+                $recipients = array ($to, $cc);
+                $mime = new Mail_Mime();
+                $mime->setTXTBody($body);
+                $headers = array(
+                    "Reply-To" => "<membership-committee@gnome.org>",
+                    "From" => "GNOME Foundation Membership Committee <membership-committee@gnome.org>",
+                    "To" => $to,
+                    "Cc" => $cc,
+                    "Subject" => $subject,
+                );
+                $params = array(
+                    'head_charset' => 'UTF-8',
+                    'head_encoding' => 'quoted-printable',
+                    'text_charset' => 'UTF-8',
+                );
+                $content = $mime->get($params);
+                $headers = $mime->headers($headers);
+                $mail = &Mail::factory('smtp');
+                $error = $mail->send($recipients, $headers, $content);
+                if(PEAR::isError($error))
+                    return $error;
+                
+                $updatednode = $formnode->appendChild($dom->createElement('emailsent'));
+                
+            }
 
-		return;
-	}
-	
-	function readform() {
-		// Read details from form
-		$this->foundationmember->firstname = $_POST['firstname'];
-		$this->foundationmember->lastname = $_POST['lastname'];
-		$this->foundationmember->email = $_POST['email'];
-		$this->foundationmember->comments = $_POST['comments'];
-		if (isset($_POST['renew']) && $_POST['renew'] == "on") {
-			$this->foundationmember->renew = true;
-		  $this->foundationmember->last_renewed_on = time();
-		}
-		
+            $updatednode = $formnode->appendChild($dom->createElement("updated"));
+            foreach($result as $change) {
+                $node = $updatednode->appendChild($dom->createElement("change"));
+                $node->setAttribute("id", $change);
+            }
+        }
 
-		// Validate details
-		$errors = $this->foundationmember->validate();
+        return;
+    }
+    
+    function readform() {
+        // Read details from form
+        $this->foundationmember->firstname = $_POST['firstname'];
+        $this->foundationmember->lastname = $_POST['lastname'];
+        $this->foundationmember->email = $_POST['email'];
+        $this->foundationmember->comments = $_POST['comments'];
+        if (isset($_POST['renew']) && $_POST['renew'] == "on") {
+            $this->foundationmember->renew = true;
+            $this->foundationmember->last_renewed_on = time();
+        }
 
-		return $errors;
-	}
+        // Validate details
+        $errors = $this->foundationmember->validate();
+
+        return $errors;
+    }
 }
 
 require_once("common.php");
