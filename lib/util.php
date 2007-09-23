a2b8369ad523e9485c7d9de54fb4d50304cb1dd9
Olav Vitters <ovitters@src.gnome.org>
2007-09-23 11:08:53 +0000

    Older ssh-keygens allowed DSA keys other than 1024 bits. Allow everything
    	* lib/util.php: Older ssh-keygens allowed DSA keys other than 1024
    	bits. Allow everything with 1024 or more bits. Not sure if they'll
    	allow a login though.
    
    
    svn path=/trunk/; revision=101

--- lib/util.php
+++ lib/util.php
@@ -19,8 +19,10 @@ function is_valid_ssh_pub_key($key, $check_length = True, $return_fingerprint = 
     if ($check_length) {
         # DSA certificate data is exactly 433 bytes (always 1024 bits, comparable to 1536 RSA key, has 305 of other data)
         # RSA has to be >= 277 bytes (2048 bits, 21 bytes of other data)
+        #
+        # However, old ssh-keugen versions allowed DSA keys with != 1024 bits...
         $cert_length = strlen($certificate);
-        if (($format == "ssh-dss" && $cert_length != 433)
+        if (($format == "ssh-dss" && $cert_length < 433)
             || ($format == "ssh-rsa" && $cert_length < 277))
         {
             # Either invalid, or not enough bits in the public key
