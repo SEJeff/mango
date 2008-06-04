461dd29324c7b5a502ecfed744e7bae8c31a18dc
Olav Vitters <ovitters@src.gnome.org>
2008-06-04 06:47:33 +0000

    Retab!
    svn path=/trunk/; revision=194

--- www/update_module.php
+++ www/update_module.php
@@ -10,137 +10,136 @@ define('GROUP', 'sysadmin');
 
 
 class UpdateModule {
-	var $module;
+    var $module;
 
-	// An initialisation error message
-	var $error;
-		
-	function UpdateModule($cn) {
+    // An initialisation error message
+    var $error;
 
-		$module = Module::fetchmodule($cn);
-		if(!$module instanceof Module) {
-			$this->error = $module;
-			return;
-		}
+    function UpdateModule($cn) {
 
-		$this->module = $module;
-	}
-		
-       static function main() {
-		// Check session for previous instance
-		$container = isset($_SESSION[SESSIONID]) ? $_SESSION[SESSIONID] : null;
-		if(!$container instanceof UpdateModule || isset($_REQUEST['cn'])) {
-			$cn = $_REQUEST['cn'];
-			$container = new UpdateModule($cn);
-			if($container->error) {
-				Page::redirect("/list_moduls.php?errmsg=".urlencode($container->error));
-				return;
-			}
-			$_SESSION[SESSIONID] = $container;
-		}
+        $module = Module::fetchmodule($cn);
+        if(!$module instanceof Module) {
+            $this->error = $module;
+            return;
+        }
 
-		// Set up a page for tracking the response for this request
-		$page = new Page(STYLESHEET);
-		
-		// Service the request, tracking results and output on the given DOM
-		$container->service($page->result);
-		
-		// Send the page for post-processing and output
-		$page->send();
-		
-		// Save anything changed in the session
-		$_SESSION[SESSIONID] = $container;
-	}
-	
-	function service(&$dom) {
-		// A page node is mandatory
-		$dom->appendChild($pagenode = $dom->createElement("page"));
-		$pagenode->setAttribute("title", "Update Module '".$this->module->cn."'");
+        $this->module = $module;
+    }
 
-		// Security check
-		if(!check_permissions($dom, $pagenode, GROUP)) return;
-		
-		// Start the page off
-		$formnode = $pagenode->appendChild($dom->createElement("updatemodule"));
+    static function main() {
+        // Check session for previous instance
+        $container = isset($_SESSION[SESSIONID]) ? $_SESSION[SESSIONID] : null;
+        if(!$container instanceof UpdateModule || isset($_REQUEST['cn'])) {
+            $cn = $_REQUEST['cn'];
+            $container = new UpdateModule($cn);
+            if($container->error) {
+                Page::redirect("/list_moduls.php?errmsg=".urlencode($container->error));
+                return;
+            }
+            $_SESSION[SESSIONID] = $container;
+        }
 
-		// If posting details, attempt to add the new details
-		if($_SERVER['REQUEST_METHOD'] == 'POST') {
-			$this->process($dom, $formnode);
-		}
-		
-		// Add current details to form
-		$this->module->add_to_node($dom, $formnode);
-		
-		// Add initialisation error, if any
-		if(PEAR::isError($this->error)) {
-			$node = $formnode->appendChild($dom->createElement("error"));
-			$node->appendChild($dom->createTextNode($this->error->getMessage()));
-		}
+        // Set up a page for tracking the response for this request
+        $page = new Page(STYLESHEET);
 
-		return;
-	}
+        // Service the request, tracking results and output on the given DOM
+        $container->service($page->result);
 
-	function process(&$dom, &$formnode) {	
-		// Check ref (in case of multiple open pages)
-/*		$cncheck = $_POST['cncheck'];
-		if($this->module->cn != $cncheck) {
-			$module = Module::fetchmodule($cn);
-			if(!is_a($module, "Module")) {
-				$this->error = $module;
-				return;
-			}
-			$this->module = $module;
-		}
+        // Send the page for post-processing and output
+        $page->send();
+
+        // Save anything changed in the session
+        $_SESSION[SESSIONID] = $container;
+    }
+
+    function service(&$dom) {
+        // A page node is mandatory
+        $dom->appendChild($pagenode = $dom->createElement("page"));
+        $pagenode->setAttribute("title", "Update Module '".$this->module->cn."'");
+
+        // Security check
+        if(!check_permissions($dom, $pagenode, GROUP)) return;
+
+        // Start the page off
+        $formnode = $pagenode->appendChild($dom->createElement("updatemodule"));
+
+        // If posting details, attempt to add the new details
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $this->process($dom, $formnode);
+        }
+
+        // Add current details to form
+        $this->module->add_to_node($dom, $formnode);
+
+        // Add initialisation error, if any
+        if(PEAR::isError($this->error)) {
+            $node = $formnode->appendChild($dom->createElement("error"));
+            $node->appendChild($dom->createTextNode($this->error->getMessage()));
+        }
+
+        return;
+    }
+
+    function process(&$dom, &$formnode) {   
+        // Check ref (in case of multiple open pages)
+/*      $cncheck = $_POST['cncheck'];
+        if($this->module->cn != $cncheck) {
+            $module = Module::fetchmodule($cn);
+            if(!is_a($module, "Module")) {
+                $this->error = $module;
+                return;
+            }
+            $this->module = $module;
+        }
 */
-		// Individual tab form handlers
-		$result = null;
+        // Individual tab form handlers
+        $result = null;
 
-		// Report an exception
-		if(PEAR::isError($result)) {
-			$node = $formnode->appendChild($dom->createElement("error"));
-			$node->appendChild($dom->createTextNode($result->getMessage()));
-			return;
-		}
-				// Read form and validate
-		$this->module->cn = $_POST['cn'];
-		$this->module->sn = $_POST['cn'];
-		$this->module->maintainerUids = $_POST['maintainerUids'];
-		$this->module->description = $_POST['description'];
-		$this->module->localizationModule = $_POST['localizationModule'];
-		if ($this->module->localizationModule) { 
-			$this->module->localizationTeam = $_POST['localizationTeam'];
-			$this->module->mailingList = $_POST['mailingList'];
-		} else {
-			$this->module->localizationModule = '';
-			$this->module->localizationTeam = '';
-		}
-		$formerrors = $this->module->validate();
-		if(count($formerrors) > 0) {
-			foreach($formerrors as $error) {
-				$node = $formnode->appendChild($dom->createElement("formerror"));
-				$node->setAttribute("type", $error);
-			}
-			return;
-		}
+        // Report an exception
+        if(PEAR::isError($result)) {
+            $node = $formnode->appendChild($dom->createElement("error"));
+            $node->appendChild($dom->createTextNode($result->getMessage()));
+            return;
+        }
+        // Read form and validate
+        $this->module->cn = $_POST['cn'];
+        $this->module->sn = $_POST['cn'];
+        $this->module->maintainerUids = $_POST['maintainerUids'];
+        $this->module->description = $_POST['description'];
+        $this->module->localizationModule = $_POST['localizationModule'];
+        if ($this->module->localizationModule) { 
+            $this->module->localizationTeam = $_POST['localizationTeam'];
+            $this->module->mailingList = $_POST['mailingList'];
+        } else {
+            $this->module->localizationModule = '';
+            $this->module->localizationTeam = '';
+        }
+        $formerrors = $this->module->validate();
+        if(count($formerrors) > 0) {
+            foreach($formerrors as $error) {
+                $node = $formnode->appendChild($dom->createElement("formerror"));
+                $node->setAttribute("type", $error);
+            }
+            return;
+        }
 
-		// Attempt LDAP update
-		$result = $this->module->update();
-		if(PEAR::isError($result))
-			return $result;
+        // Attempt LDAP update
+        $result = $this->module->update();
+        if(PEAR::isError($result))
+            return $result;
 
-		// Mark success
-		$updatednode = $formnode->appendChild($dom->createElement("updated"));
-		$updatednode->appendChild($node = $dom->createElement('cn'));
-		$node->appendChild($dom->createTextNode($this->module->cn));
-		// Report successes
-		if(is_array($result)) {
-			foreach($result as $change) {
-				$node = $formnode->appendChild($dom->createElement("changed"));
-				$node->setAttribute("cn", $change);
-			}
-		}
-
-	}
+        // Mark success
+        $updatednode = $formnode->appendChild($dom->createElement("updated"));
+        $updatednode->appendChild($node = $dom->createElement('cn'));
+        $node->appendChild($dom->createTextNode($this->module->cn));
+        // Report successes
+        if(is_array($result)) {
+            foreach($result as $change) {
+                $node = $formnode->appendChild($dom->createElement("changed"));
+                $node->setAttribute("cn", $change);
+            }
+        }
+    }
 }
 
 require_once("common.php");
