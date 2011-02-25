5c9f8c1d2a614afa8e3de1c8972f522619ef56db
Jeff Schroeder <jeffschroeder@computer.org>
2011-02-24 18:12:52 -0800

    Trim down the settings.py quite a bit

--- mango/settings.py
+++ mango/settings.py
@@ -9,12 +9,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'mango'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'mango.db'   # Or path to database file if using sqlite3.
+DATABASE_USER = ''    # Not used with sqlite3.
+DATABASE_PASSWORD = ''    # Not used with sqlite3.
+DATABASE_HOST = ''        # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''        # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -23,8 +23,6 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # system time zone.
 TIME_ZONE = 'UTC'
 
-
-SITE_ROOT = r'mango/'
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
@@ -56,7 +54,6 @@ SECRET_KEY = 'u(9-ndez*a1wi-438du+!rx$o+7nntz1w$tb0i_3m)+w$jt=sh'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
 )
 
 SESSION_ENGINE = "django.contrib.sessions.backends.file"
@@ -85,72 +82,13 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-#    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'users',
 )
-
-
-cfg_opts = set((
-    'cached_date',       # Date config last read from disk
-    'mode',              # Runtime mode (Live/Preview/Development)
-    'base_url',          # Base URL
-
-    'accounts_db_url',   # Mirrors MySQL database URL
-    'mirrors_db_url',    # Mirrors MySQL database URL
-    'membership_db_url', # Foundation membership MySQL database URL
-
-    'mail_backend',       # Mail backend
-    'mail_sendmail_path', # Path to sendmail (sendmail backend)
-    'mail_sendmail_args', # Additional options for sendmail (sendmail backend)
-    'mail_smtp_host',     # SMTP server hostname (smtp backend)
-    'mail_smtp_port',     # SMTP server port (smtp backend)
-    'mail_smtp_auth',     # Whether or not to use smtp authentication (smtp backend)
-    'mail_smtp_username', # Username to use for SMTP authentication (smtp backend)
-    'mail_smtp_password', # Password to use for SMTP authentication (smtp backend)
-    'mail_smtp_localhost', # Value to give when sending EHLO or HELO (smtp backend)
-    'mail_smtp_timeout',  # SMTP connection timeout
-    'mail_smtp_persist',  # Whether or not to use persistent SMTP connections (smtp backend)
-
-    'ldap_url',            # LDAP URL
-    'ldap_binddn',         # LDAP bind DN
-    'ldap_bindpw',         # LDAP bind PW
-    'ldap_basedn',         # LDAP base DN
-    'ldap_users_basedn',   # LDAP users base DN
-    'ldap_groups_basedn',  # LDAP groups base DN
-    'ldap_modules_basedn', # LDAP modules base DN
-    'ldap_aliases_basedn', # LDAP aliases base DN
-
-    'token_salt',          # Salt to be used in e-mail tokens
-
-    'support_email',       # Support e-mail
-    'account_email',       # Email address of person(s) who handles account management -->
-
-    'session_path'        # Session save path
-))
-
-MANGO_CFG = {}
-
-try:
-    import xml.etree.cElementTree as et
-except ImportError:
-    import cElementTree as et
-
-cfg_files = ('/var/www/mango/config.xml', '/etc/mango/config.xml')
-
-import os.path
-for f in cfg_files:
-    if os.path.exists(f):
-        doc = et.parse(file(f, 'r'))
-        root = doc.getroot()
-        for el in root.getchildren():
-            if el.tag in cfg_opts:
-                MANGO_CFG[el.tag] = el.text
-        
-        break
-
-MANGO_CFG['base_url'] = 'http://localhost/mango'
 
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
