bdce78133cac108e5deea2cc3234c3e45d3d918d
Jeff Schroeder <jeffschroeder@computer.org>
2011-02-24 18:43:07 -0800

    s/NewUserForm/UpdateUserForm/ and fix the user list table width

--- mango/users/views.py
+++ mango/users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from models import LdapUser, LdapGroup
-from forms import NewUserForm
+from forms import UpdateUserForm
 
 def index(request, template="users/index.html"):
     users = LdapUser.objects.all()
