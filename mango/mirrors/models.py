d96178fe6554dc191092d0f022ae2f594f192686
Jeff Schroeder <jeffschroeder@computer.org>
2011-02-26 11:09:30 -0800

    Start cleaning up the models in the mirrors app

--- mango/mirrors/models.py
+++ mango/mirrors/models.py
@@ -1,8 +1,5 @@
-
 from django.db import models
 from django.conf import settings
-from django.utils import tree
-from django.db.models import Q
 
 LOCATION_CHOICES = (
     ('United States and Canada', 'United States and Canada'),
@@ -14,7 +11,6 @@ LOCATION_CHOICES = (
 )
 
 class Ftpmirrors(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=60)
     url = models.URLField(verify_exists=False)
     location = models.CharField(max_length=72, choices=LOCATION_CHOICES)
@@ -26,16 +22,6 @@ class Ftpmirrors(models.Model):
     class Meta:
         db_table = u'ftpmirrors'
 
-    def add_to_xml(self, ET, node):
-        fields = ('id', 'name', 'url', 'location', 'email', 'description', 'comments', 'last_update')
-        for field in fields:
-            n = ET.SubElement(node, field)
-            val = getattr(self, field)
-            if val is None: val = ''
-            n.text = unicode(val)
-        if self.active:
-            n = ET.SubElement(node, 'active')
-
 class Webmirrors(models.Model):
     name = models.CharField(max_length=60, blank=True)
     url = models.CharField(max_length=300, blank=True)
@@ -43,7 +29,6 @@ class Webmirrors(models.Model):
     email = models.CharField(max_length=120, blank=True)
     comments = models.TextField(blank=True)
     description = models.TextField(blank=True)
-    id = models.IntegerField(primary_key=True)
     active = models.IntegerField(null=True, blank=True)
     class Meta:
         db_table = u'webmirrors'
