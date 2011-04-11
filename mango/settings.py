37c2031571f84af99aa321ecf2be66e1ec2c4f9b
Jeff Schroeder <jeffschroeder@computer.org>
2011-04-10 22:26:41 -0700

    Enabling the mirrors app dbrouter

--- mango/settings.py
+++ mango/settings.py
@@ -10,12 +10,21 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'mango.db'   # Or path to database file if using sqlite3.
-DATABASE_USER = ''    # Not used with sqlite3.
-DATABASE_PASSWORD = ''    # Not used with sqlite3.
-DATABASE_HOST = ''        # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''        # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME'  : os.path.join(os.path.dirname(__file__), "mango.db")
+    },
+    # For the mirrors application
+    #'gnome_mirrors': {
+    #    'ENGINE'    : 'django.db.backends.mysql',
+    #    'NAME'      : 'mirrors',
+    #    'USER'      : 'dbuser',
+    #    'PASSWORD'  : 'dbpass',
+    #},
+}
+
+DATABASE_ROUTERS = [ "mango.mirrors.routers.MirrorRouter", ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
