cc98d925a91949f66e0bc3daa024d513b2d3f508
Jeff Schroeder <jeffschroeder@computer.org>
2011-04-23 21:46:41 -0700

    Add FtpMirror.get_absolute_url()

--- mango/mirrors/models.py
+++ mango/mirrors/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.template.defaultfilters import slugify
 
 LOCATION_CHOICES = (
     ('United States and Canada', 'United States and Canada'),
@@ -23,10 +25,14 @@ class FtpMirror(models.Model):
     class Meta:
         db_table = u'ftpmirrors'
     def __unicode__(self):
-        return "%s Ftpmirror (%s)" % (self.name.title(), self.url)
+        return "%s FtpMirror (%s)" % (self.name.title(), self.url)
 
     def is_active(self):
         return self.active == 1
+
+    def get_absolute_url(self):
+        return reverse("mirrors-update", args=(self.pk, slugify(self.name)))
+
 
 class Webmirror(models.Model):
     name = models.CharField(max_length=60, blank=True)
