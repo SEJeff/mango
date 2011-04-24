9a3f8dbe9b3e58d1f45be1598ea1a0abc00eec9a
Jeff Schroeder <jeffschroeder@computer.org>
2011-04-24 10:53:30 -0700

    Switch the mirrors app over to use django-uni-form for more pretty

--- mango/mirrors/forms.py
+++ mango/mirrors/forms.py
@@ -1,6 +1,14 @@
 from models import FtpMirror
 from django.forms import ModelForm
+from uni_form.helpers import FormHelper, Submit
+from django.utils.translation import ugettext_lazy as _
 
 class FtpMirrorForm(ModelForm):
+
+    # django-uni-form stuff to make the pretty
+    helper = FormHelper()
+    submit = Submit('submit', _('Submit Changes'))
+    helper.add_input(submit)
+
     class Meta:
         model = FtpMirror
