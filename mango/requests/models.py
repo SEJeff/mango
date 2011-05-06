aea177fc5abaa5807c8e3d1958165c0a79743fa6
Jeff Schroeder <jeffschroeder@computer.org>
2011-05-05 23:03:55 -0700

    Update request.models to make things a bit more friendly
        - Add a "choices" attribute to the 2 models to enforce
          the proper choices are selected in the admin or app.
        - Add __unicode__() to AccountGroup and AccountRequest
        - s/AccountGroups/AccountGroup/ to follow the django conventions

--- mango/requests/models.py
+++ mango/requests/models.py
@@ -1,28 +1,44 @@
 from django.db import models
 from django.conf import settings
 
+REQUEST_VERDICTS = (
+  ('A', 'approved'),
+  ('R', 'rejected'),
+  ('M', 'mail_verification'),
+  ('V', 'awaiting_vouchers'),
+  ('S', 'awaiting_setup'),
+  ('P', 'pending'),
+)
+
 class AccountRequest(models.Model):
-    id = models.AutoField(primary_key=True)
     uid = models.CharField(max_length=15)
     cn = models.CharField(max_length=255)
     mail = models.EmailField(max_length=255)
     comment = models.TextField()
     timestamp = models.DateTimeField(editable=False)
     authorizationkeys = models.TextField()
-    status = models.CharField(max_length=1, default='P', editable=False)
+    status = models.CharField(max_length=1, default='P', choices=REQUEST_VERDICTS, editable=False)
+    mail_token = models.CharField(max_length=40, editable=False)
+    # FIXME: Original schema designers were on crack. These should be BooleanFields
     is_new_account = models.CharField(max_length=1, default='Y', editable=False)
     is_mail_verified = models.CharField(max_length=1, default='N', editable=False)
-    mail_token = models.CharField(max_length=40, editable=False)
+
     class Meta:
         db_table = u'account_request'
 
-class AccountGroups(models.Model):
-    id = models.AutoField(primary_key=True)
+    def __unicode__(self):
+        return u"<%s %s from %s>" % (self.get_status_display(), self.__class__.__name__, self.uid)
+
+class AccountGroup(models.Model):
     request = models.ForeignKey(AccountRequest)
     cn = models.CharField(max_length=15)
     voucher_group = models.CharField(max_length=50, blank=True, null=True)
-    verdict = models.CharField(max_length=1, default='P', editable=False)
+    verdict = models.CharField(max_length=1, default='P', choices=REQUEST_VERDICTS, editable=False)
     voucher = models.CharField(max_length=15, blank=True, null=True)
     denial_message = models.CharField(max_length=255, blank=True, null=True)
+
     class Meta:
         db_table = u'account_groups'
+
+    def __unicode__(self):
+        return u"<%s %s from %s for %s>" % (self.get_verdict_display().title(), self.__class__.__name__, self.request.uid, self.voucher_group)
