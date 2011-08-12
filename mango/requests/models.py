3e7ee2e7e53b29047b4dd2a3860c355a35789a48
Jeff Schroeder <jeffschroeder@computer.org>
2011-08-12 05:14:03 -0700

    Code purtification on requests.models

--- mango/requests/models.py
+++ mango/requests/models.py
@@ -13,16 +13,16 @@ REQUEST_VERDICTS = (
 
 class AccountRequest(models.Model):
     uid = models.CharField(max_length=15)
-    cn = models.CharField(max_length=255)
-    mail = models.EmailField(max_length=255)
-    comment = models.TextField()
-    timestamp = models.DateTimeField(editable=False)
+    cn                = models.CharField(max_length=255)
+    mail              = models.EmailField(max_length=255)
+    comment           = models.TextField()
+    timestamp         = models.DateTimeField(editable=False)
     authorizationkeys = models.TextField()
-    status = models.CharField(max_length=1, default='P', choices=REQUEST_VERDICTS, editable=False)
-    mail_token = models.CharField(max_length=40, editable=False)
+    status            = models.CharField(max_length=1, default='P', choices=REQUEST_VERDICTS, editable=False)
+    mail_token        = models.CharField(max_length=40, editable=False)
     # FIXME: Original schema designers were on crack. These should be BooleanFields
-    is_new_account = models.CharField(max_length=1, default='Y', editable=False)
-    is_mail_verified = models.CharField(max_length=1, default='N', editable=False)
+    is_new_account    = models.CharField(max_length=1, default='Y', editable=False)
+    is_mail_verified  = models.CharField(max_length=1, default='N', editable=False)
 
     class Meta:
         db_table = u'account_request'
@@ -31,11 +31,11 @@ class AccountRequest(models.Model):
         return _("%s account request for %s") % (self.get_status_display().title(), self.uid)
 
 class AccountGroup(models.Model):
-    request = models.ForeignKey(AccountRequest)
-    cn = models.CharField(max_length=15)
-    voucher_group = models.CharField(max_length=50, blank=True, null=True)
-    verdict = models.CharField(max_length=1, default='P', choices=REQUEST_VERDICTS, editable=False)
-    voucher = models.CharField(max_length=15, blank=True, null=True)
+    request        = models.ForeignKey(AccountRequest)
+    cn             = models.CharField(max_length=15)
+    voucher_group  = models.CharField(max_length=50, blank=True, null=True)
+    verdict        = models.CharField(max_length=1, default='P', choices=REQUEST_VERDICTS, editable=False)
+    voucher        = models.CharField(max_length=15, blank=True, null=True)
     denial_message = models.CharField(max_length=255, blank=True, null=True)
 
     def for_display(self):
